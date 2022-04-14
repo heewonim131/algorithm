2e61612ad165c0a62e9fa7bbb4e4e2f9a82c150e
heewonim131 <heewonim131@gmail.com>
2022-04-14 20:31:06 +0900

    Programmers: 순열 검사

---
+++ src/Programmers/순열_검사.java
@@ -0,0 +1,29 @@
+package Programmers;
+
+public class 순열_검사 {
+    public static void main(String[] args) {
+        int[] arr = {4, 1, 3, 2};
+        System.out.println(solution(arr));
+    }
+
+    public static boolean solution(int[] arr) {
+        boolean answer = true;
+        int[] chk = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0 || arr[i] > arr.length){
+                answer = false;
+                break;
+            }
+            chk[arr[i]-1]++;
+        }
+
+        for (int i = 0; i < chk.length; i++) {
+            if (chk[i] != 1){
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
