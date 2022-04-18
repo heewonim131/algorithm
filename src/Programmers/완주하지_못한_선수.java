3e06a383ca177ccaff7f73c756dcfeaea78e37d7
heewonim131 <heewonim131@gmail.com>
2022-04-18 15:02:27 +0900

    Programmers: 완주하지 못한 선수

---
+++ src/Programmers/완주하지_못한_선수.java
@@ -0,0 +1,25 @@
+package Programmers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 완주하지_못한_선수 {
+    public static void main(String[] args) {
+        String[] participant = {"mislav", "stanko", "mislav", "ana"};
+        String[] completion = {"stanko", "ana", "mislav"};
+        System.out.println(solution(participant, completion));
+    }
+
+    public static String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String p : participant) map.put(p, map.getOrDefault(p, 0)+1);
+        for (String c : completion) {
+            int n = map.get(c) - 1;
+            if(n == 0) map.remove(c);
+            else map.put(c, n);
+        }
+
+        return map.keySet().iterator().next();
+    }
+}
